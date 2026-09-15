eb261565c43300f8a41c30a9f8d3dbdae2298959
Zihan Song <502285530@qq.com>
2026-09-15 14:01:44 +0800

    RMSNorm

--- model/model.py
+++ model/model.py
@@ -70,3 +70,14 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+class RMSNorm(nn.Module):
+    def __init__(self,dim: int, eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.dim = dim
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x: torch.Tensor) -> torch.Tensor:
+        return  torch.rsqrt(x.pow(2).mean(-1, keepdim=True)+self.eps)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.weight * x * self._norm(x)
